ce454ff1d24311d9c9281a99c432a8a30efdb484
rustekapl <rustekapl@gmail.com>
2023-06-14 13:47:26 +0200

    domain done

--- src/main/java/ru/javarush/domain/FilmText.java
+++ src/main/java/ru/javarush/domain/FilmText.java
@@ -1,5 +1,7 @@
 package ru.javarush.domain;
 
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 
 @Entity
@@ -15,4 +17,40 @@ public class FilmText {
     private Film film;
 
     private String title;
+
+    @Column(columnDefinition = "text")
+    @Type(type = "text")
+    private String description;
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
